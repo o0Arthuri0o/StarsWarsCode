430f32385bb7809179b63b7c97683a0d54a0cda1
B2441_KhussainovAA <nevisipaus@gmail.com>
2023-12-25 23:27:40 +0300

    second commit

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,60 +1,93 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import './Main.css'
 import Card from '../Card/Card'
 import Loader from '../Loader/Loader'
 const Main = () => {
 
-  const [currentPage, setCurrentPage] = useState({pageNumber:1, characters:[], nextLink: ''})
-  const [nextPage, setNextPage] = useState('https://swapi.dev/api/people/')
-
-  useEffect(()=> {
-
-    const getInfo = async() => {      
-      const response = await  fetch(nextPage);
-      const data = await response.json()
-      console.log(data.results)
-      setCurrentPage({...currentPage, characters: data.results, nextLink: data.next})
+  const allLinks = [
+    {
+      pageNumber: 1,
+      link:'https://swapi.dev/api/people'
+    },
+    {
+      pageNumber: 2,
+      link:'https://swapi.dev/api/people/?page=2'
+    },
+    {
+      pageNumber: 3,
+      link:'https://swapi.dev/api/people/?page=3'
+    },
+    {
+      pageNumber: 4,
+      link:'https://swapi.dev/api/people/?page=4'
+    },
+    {
+      pageNumber: 5,
+      link:'https://swapi.dev/api/people/?page=5'
+    },
+    {
+      pageNumber: 6,
+      link:'https://swapi.dev/api/people/?page=6'
+    },
+    {
+      pageNumber: 7,
+      link:'https://swapi.dev/api/people/?page=7'
+    },
+    {
+      pageNumber: 8,
+      link:'https://swapi.dev/api/people/?page=8'
+    },
+    {
+      pageNumber: 9,
+      link:'https://swapi.dev/api/people/?page=9'
     }
-    getInfo()
+  ]
 
-  }, [nextPage])
-  const changePage = (num) => {
-    console.log(num)
-    setNextPage(currentPage.nextLink)
-    setCurrentPage({...currentPage, pageNumber: +num})
-  }
+  const [currentPage, setCurrentPage] = useState({pageNumber: 1, info:[]})
+  useEffect(() => {
+
+    const getInfo = async() => {
+      const newPage = allLinks.filter(page => page.pageNumber === currentPage.pageNumber)[0]
+      const res = await fetch(newPage.link)
+      const data = await res.json()
+      
+    }
+
+    getInfo()
+  }, [currentPage])
 
   return (
     <div className='main-page'>
         
-        {currentPage.characters.length ? 
+        {/* {currentPage.info.length ? 
           <>
             <div className='cards-wrapper' >
-              {currentPage.characters.map((character) => 
+              {currentPage.info.map((character) => 
                 <Card name={character.name} birth={character.birth_year} gender={character.gender} />
               )}
             </div>
 
             <div className='pagination'>
-              <div onClick={(e) => changePage(e.target.textContent)} className={1 === currentPage.pageNumber ? 'page active' : 'page'}>1</div>
-              <div onClick={(e) => changePage(e.target.textContent)} className={2 === currentPage.pageNumber ? 'page active' : 'page'}>2</div>
-              <div onClick={(e) => changePage(e.target.textContent)} className={3 === currentPage.pageNumber ? 'page active' : 'page'}>3</div>
-              <div onClick={(e) => changePage(e.target.textContent)} className={4 === currentPage.pageNumber ? 'page active' : 'page'}>4</div>
-              <div onClick={(e) => changePage(e.target.textContent)} className={5 === currentPage.pageNumber ? 'page active' : 'page'}>5</div>
-              <div onClick={(e) => changePage(e.target.textContent)} className={6 === currentPage.pageNumber ? 'page active' : 'page'}>6</div>
-              <div onClick={(e) => changePage(e.target.textContent)} className={7 === currentPage.pageNumber ? 'page active' : 'page'}>7</div>
-              <div onClick={(e) => changePage(e.target.textContent)} className={8 === currentPage.pageNumber ? 'page active' : 'page'}>8</div>
-              <div onClick={(e) => changePage(e.target.textContent)} className={9 === currentPage.pageNumber ? 'page active' : 'page'}>9</div>
+              <div onClick={()=>{}} className={1 === currentPage.pageNumber ? 'page active' : 'page'}>1</div>
+              <div onClick={()=>{}} className={2 === currentPage.pageNumber ? 'page active' : 'page'}>2</div>
+              <div onClick={()=>{}} className={3 === currentPage.pageNumber ? 'page active' : 'page'}>3</div>
+              <div onClick={()=>{}} className={4 === currentPage.pageNumber ? 'page active' : 'page'}>4</div>
+              <div onClick={()=>{}} className={5 === currentPage.pageNumber ? 'page active' : 'page'}>5</div>
+              <div onClick={()=>{}} className={6 === currentPage.pageNumber ? 'page active' : 'page'}>6</div>
+              <div onClick={()=>{}} className={7 === currentPage.pageNumber ? 'page active' : 'page'}>7</div>
+              <div onClick={()=>{}} className={8 === currentPage.pageNumber ? 'page active' : 'page'}>8</div>
+              <div onClick={()=>{}} className={9 === currentPage.pageNumber ? 'page active' : 'page'}>9</div>
             </div> 
           </>
       
         :
          <Loader/>
-      }
+      } */}
 
-       
+        <Loader/>
 
     </div>
+
   )
 }
 
